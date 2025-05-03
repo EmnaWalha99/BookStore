6f7c93b24579078a612f728784fe7794a75ebc23
EmnaWalha99 <ewalha12@gmail.com>
2025-05-03 20:14:10 +0100

    BookRepository

---
+++ src/main/java/com/example/Bookstore/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.Bookstore.repositories;
+
+import com.example.Bookstore.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
