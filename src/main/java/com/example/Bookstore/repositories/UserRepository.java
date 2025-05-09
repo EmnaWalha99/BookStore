2d64f064e46a146bac16b24d1e647509d54823ae
EmnaWalha99 <ewalha12@gmail.com>
2025-05-09 21:21:31 +0100

    fixed repo

--- src/main/java/com/example/Bookstore/repositories/UserRepository.java
+++ src/main/java/com/example/Bookstore/repositories/UserRepository.java
@@ -3,11 +3,11 @@ package com.example.Bookstore.repositories;
 import com.example.Bookstore.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByEmail(String email);
 
-
-
-
+    Optional<User> findByUsername(String username);
 }
